ccb38a5a1a830ca387426814374a1f1a55c55134
Timhojin <ihavealreadylost@gmail.com.>
2024-08-18 20:26:09 +1000

    Finished building the UI and 2 number calculation in JS.
    Will add the remaining AC, DEL operations and continuing
    calculations.

--- script.js
+++ script.js
@@ -34,7 +34,63 @@ function operate(operator, firstNum, secondNum) {
     }
 }
 
+function getDisplayValue(x) {
+    const id = parseInt(x);
+    displayValue = displayValue * 10 + id;
+    onScreen.textContent = displayValue;
+}
+
+function prepareForOperation(x) {
+    operator = x;
+    firstNum = displayValue;
+    displayValue = 0;
+    onScreen.textContent = "";
+}
+
+function displayResult() {
+    secondNum = displayValue;
+    displayValue = 0;
+    let result = operate(operator, firstNum, secondNum);
+    onScreen.textContent = result;
+    firstNum = result;
+}
+
 let firstNum = 0;
 let secondNum = 0;
 let operator = '';
 
+let displayValue = 0;
+const onScreen = document.querySelector(".on-screen");
+
+const one = document.querySelector(".one");
+one.addEventListener("click", () => getDisplayValue(one.id));
+const two = document.querySelector(".two");
+two.addEventListener("click", () => getDisplayValue(two.id));
+const three = document.querySelector(".three");
+three.addEventListener("click", () => getDisplayValue(three.id));
+const four = document.querySelector(".four");
+four.addEventListener("click", () => getDisplayValue(four.id));
+const five = document.querySelector(".five");
+five.addEventListener("click", () => getDisplayValue(five.id));
+const six = document.querySelector(".six");
+six.addEventListener("click", () => getDisplayValue(six.id));
+const seven = document.querySelector(".seven");
+seven.addEventListener("click", () => getDisplayValue(seven.id));
+const eight = document.querySelector(".eight");
+eight.addEventListener("click", () => getDisplayValue(eight.id));
+const nine = document.querySelector(".nine");
+nine.addEventListener("click", () => getDisplayValue(nine.id));
+const zero = document.querySelector(".zero");
+zero.addEventListener("click", () => getDisplayValue(zero.id));
+
+const addButton = document.querySelector(".add");
+addButton.addEventListener("click", () => {prepareForOperation('+')});
+const subtractButton = document.querySelector(".subtract");
+subtractButton.addEventListener("click", () => {prepareForOperation('-')});
+const multiplyButton = document.querySelector(".multiply");
+multiplyButton.addEventListener("click", () => {prepareForOperation('*')});
+const divideButton = document.querySelector(".divide");
+divideButton.addEventListener("click", () => {prepareForOperation('/')});
+
+const equalButton = document.querySelector(".equal");
+equalButton.addEventListener("click", () => displayResult());
